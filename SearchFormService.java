a92ed81f5f9a59763da43c2981c67b5a662e8708
David Riley <65918616+Gatene1@users.noreply.github.com>
2022-02-15 23:22:22 -0500

    Create SearchFormService.java

---
+++ SearchFormService.java
@@ -0,0 +1,28 @@
+package com.revature.DavidRiley.Server;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class SearchFormService extends HttpServlet {
+    @Override
+    // We are overriding
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        String HTMLForm = "<html>" +
+                "          <head>" +
+                "               <title>Search Pokedex</title>" +
+                "               <meta charset='UTF-8'>" +
+                "          </head>" +
+                "          <body>" +
+                "               <form action = '/Pokemon' method = 'get'>" +
+                "                   <input type='name' name='searchName'>" +
+                "                   <input type='submit' value='search'>" +
+                "               </form>" +
+                "           </body>" +
+                "           </html>";
+        resp.getWriter().println(HTMLForm);
+    }
+}
